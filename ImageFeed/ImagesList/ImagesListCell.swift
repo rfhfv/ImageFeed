65a1e48d98202e053c75cc5e6faad1e8dcc0b635
Anna <sozontova.an@icloud.com>
2023-07-31 23:13:22 +0300

    Написала первую часть кода для показа изображений

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by admin on 24.07.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var cellImage: UIImageView!
+}
